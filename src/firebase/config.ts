f5f3b93c3278fe556de5141dae4374228726ff66
Nbarawii Juniour <tuckshopkonnect@gmail.com>
2026-01-14 15:07:24 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2857317523-117ad",
+  "appId": "1:106867145581:web:072d31bc4df02dfb16b864",
+  "apiKey": "AIzaSyBGTgoIQq9qRywnPHMzTSD8w3ElyM_k5WM",
+  "authDomain": "studio-2857317523-117ad.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "106867145581"
+};
