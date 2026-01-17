f4ad4128bb8007d7d0d8dc21c859f2c22934139d
Nbarawii Juniour <tuckshopkonnect@gmail.com>
2026-01-17 10:02:03 +0000

    ## Error Type Console Error
    ## Error Message
    An empty string ("") was p

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -23,10 +24,11 @@ Avatar.displayName = AvatarPrimitive.Root.displayName
 const AvatarImage = React.forwardRef<
   React.ElementRef<typeof AvatarPrimitive.Image>,
   React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>
->(({ className, ...props }, ref) => (
+>(({ className, src, ...props }, ref) => (
   <AvatarPrimitive.Image
     ref={ref}
     className={cn("aspect-square h-full w-full", className)}
+    src={src || undefined}
     {...props}
   />
 ))
